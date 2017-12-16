b6dac049bb1056b47a7fe295fbc0d2940bf8b4d6
Todd <tcbkc111@gmail.com>
2017-12-16 15:37:10 -0600

    changed technologies section

--- food_finder/js/setup.js
+++ food_finder/js/setup.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// this script does the main setup for the app, declares variables and runs starting functions...
+
 // item returned from search for nutrition info
 let searchedItem;
 // holds currently selected recipes index in recipes[]
@@ -8,55 +10,13 @@ let currentRecipe;
 let recipes = [];
 // make another empty array
 let savedSearchItems = [];
-
-// check for save and fill array, or create blank JSON to start with
-function savedRecipesCheck () {
-	if (window.localStorage.getItem('savedRecipes')) {
-		// load if found
-		recipes = JSON.parse(window.localStorage.getItem('savedRecipes'));
-	} else {
-		// create blank storage to avoid errors if storage is cleared
-		window.localStorage.setItem('savedRecipes', JSON.stringify(recipes));
-		savedRecipesCheck();
-	}
-}
-function savedItemsCheck () {
-	if (window.localStorage.getItem('savedSearchItems')) {
-		// load if found
-		savedSearchItems = JSON.parse(window.localStorage.getItem('savedSearchItems'));
-	} else {
-		// create blank storage to avoid errors if storage is cleared
-		window.localStorage.setItem('savedSearchItems', JSON.stringify(savedSearchItems));
-		savedItemsCheck();
-	}
-}
-savedRecipesCheck();
-savedItemsCheck();
-
-// ingredient object
-function Ingredient (item, amount) {
-	// name of ingredient
-	this.item = item;
-	// amount of item used in recipe
-	this.amount = amount;
-};
-
-// recipe object
-function Recipe (name) {
-	// name of recipe
-	this.name = name;
-	// array of ingredients stored in recipe
-	this.ingredients = [];
-};
-
 // search box text input
 let searchInput = document.getElementById('searchInput');
 // search submit button
 let searchButton = document.getElementById('searchButton');
-
 // main food item container
 let mainPanel = document.getElementsByClassName('itemContainer')[0];
-// smaller food item details container
+// smaller food item details containers
 let detailsPanelL = document.getElementsByClassName('itemDetailsPanel')[0];
 let detailsPanelR = document.getElementsByClassName('itemDetailsPanel')[1];
 // picture of current searched item
@@ -85,12 +45,10 @@ let totFat = document.getElementsByClassName('itemDetails')[7];
 let carbs = document.getElementsByClassName('itemDetails')[8];
 // and sodium
 let sodium = document.getElementsByClassName('itemDetails')[9];
-
 // overlay for failed search
 let searchFail = document.getElementsByClassName('searchFail')[0];
 // screen overlay to alert when saved
 let saveOverlay = document.getElementsByClassName('saved')[0];
-
 // new recipe button
 let newRecipeButton = document.getElementById('newRecipe');
 // button to open menu
@@ -100,7 +58,7 @@ let recipeBookPanel = document.getElementsByClassName('recipeBookPanel')[0];
 // display of currently selected recipe
 let currentRecipeName = document.getElementById('currentRecipe');
 
-// info panel for specific recipe, basically same as food item
+// info panel for specific recipe, layout is same as food item
 let recipeInfoPanel = document.getElementsByClassName('recipeContainer')[0];
 let recipeIngredients = document.getElementsByClassName('recipeIngredientsPanel')[0];
 let recipeTitle = document.getElementById('recipeTitle');
@@ -130,10 +88,52 @@ document.getElementById('renameRecipe').addEventListener('click', renameRecipe);
 // hide info panel
 document.getElementById('hideRecipe').addEventListener('click', hideRecipe);
 // press enter when typing in search box
-searchInput.addEventListener('keypress', function (event) {
-	// only key we care about is Enter
-	if (event.key === 'Enter') {
-		// call same function as clicking on search button
-		initiateSearch();
-	}
+searchInput.addEventListener('keypress', function(event) {
+  // only key we care about is Enter
+  if (event.key === 'Enter') {
+    // call same function as clicking on search button
+    initiateSearch();
+  }
 });
+
+
+// check for save and fill array, or create blank JSON to start with
+function savedRecipesCheck() {
+  if (window.localStorage.getItem('savedRecipes')) {
+    // load if found
+    recipes = JSON.parse(window.localStorage.getItem('savedRecipes'));
+  } else {
+    // create blank storage to avoid errors if storage is cleared
+    window.localStorage.setItem('savedRecipes', JSON.stringify(recipes));
+    savedRecipesCheck();
+  }
+}
+
+function savedItemsCheck() {
+  if (window.localStorage.getItem('savedSearchItems')) {
+    // load if found
+    savedSearchItems = JSON.parse(window.localStorage.getItem('savedSearchItems'));
+  } else {
+    // create blank storage to avoid errors if storage is cleared
+    window.localStorage.setItem('savedSearchItems', JSON.stringify(savedSearchItems));
+    savedItemsCheck();
+  }
+}
+savedRecipesCheck();
+savedItemsCheck();
+
+// ingredient object
+function Ingredient(item, amount) {
+  // name of ingredient
+  this.item = item;
+  // amount of item used in recipe
+  this.amount = amount;
+};
+
+// recipe object
+function Recipe(name) {
+  // name of recipe
+  this.name = name;
+  // array of ingredients stored in recipe
+  this.ingredients = [];
+};
